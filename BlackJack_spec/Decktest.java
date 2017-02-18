50aab3a28728aff5667da37fe776b43aee651a2a
Justafigurehead <strachanmichaela@hotmail.com>
2017-02-18 20:15:10 +0000

    Created initial DeckTest with one test to check DeckSize.

--- BlackJack_spec/Decktest.java
+++ BlackJack_spec/Decktest.java
@@ -2,3 +2,22 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Before;
 import BlackJack.*;
+
+public class DeckTest{
+
+  Deck deck;
+
+  @Before
+  public void before(){
+    deck = new Deck();
+  }
+
+
+// Tests for Basic Deck
+
+  
+  @Test
+  public void testEmptyDeckArray(){
+    assertEquals(0, deck.getDeckSize());
+  }
+}
