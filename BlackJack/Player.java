cb91e96a222adad588655e5d20f22f8688814a5a
Justafigurehead <strachanmichaela@hotmail.com>
2017-02-19 18:53:39 +0000

    PlayerTest includes passed test to get playerName and HandSize.

--- BlackJack/Player.java
+++ BlackJack/Player.java
@@ -1,14 +1,23 @@
 package BlackJack;
 
+import java.util.ArrayList;
+
 public class Player {
   
   private String name;
   private ArrayList<Card> hand;
 
-  public player(String name){
+  public Player(String name){
     this.name = name;
     this.hand = new ArrayList<Card>();
   }
 
 
+  public String getPlayerName(){
+    return this.name;
+  }
+
+  public int getHandSize(){
+    return this.hand.size();
+  }
 }
--- BlackJack_spec/PlayerTest.java
+++ BlackJack_spec/PlayerTest.java
@@ -1,16 +1,33 @@
 // Test player can received a card from Dealer. Allow Player to look at said card. Create player to have an array of cards so it's easier to expand app and allow player to play 21 properly. 
 // 
-import org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Before;
+import java.util.ArrayList;
 import BlackJack.*;
 
 public class PlayerTest{
 
+  private Player playerOne;
+
   @Before
   public void before(){
-    
+    playerOne = new Player("Mickie");
   }
+
+
+// Test for Player
+  @Test
+  public void getPlayerName(){
+    assertEquals("Mickie", playerOne.getPlayerName());
+  }
+
+  @Test
+  public void checkPlayerHasEmptyHand(){
+    assertEquals(0, playerOne.getHandSize());
+  }
+
+
 
 // EXPANSION
 
