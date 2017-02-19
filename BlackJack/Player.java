f64baf1d6ce1e1c4ab5c965f2cf38d6387bdd996
Justafigurehead <strachanmichaela@hotmail.com>
2017-02-19 00:14:56 +0000

    Added name and hand to player class.

--- BlackJack/Player.java
+++ BlackJack/Player.java
@@ -1,1 +1,14 @@
 package BlackJack;
+
+public class Player {
+  
+  private String name;
+  private ArrayList<Card> hand;
+
+  public player(String name){
+    this.name = name;
+    this.hand = new ArrayList<Card>();
+  }
+
+
+}
